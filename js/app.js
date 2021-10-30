bd81ebd9c79b5d4a842b79c66ce8368977d90ba2
Pruffit <kotlaevdanil@gmail.com>
2021-10-30 21:56:08 +0400

    Create JS Modals

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    /*FILTER*/
     let filter = $("[data-filter]");
     filter.on("click", function(event) {
         event.preventDefault();
@@ -24,6 +25,67 @@ $(function() {
 
         }
 
+    });
+
+    /*MODAL*/
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+
+        }, 200);
+
+    });
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+
+        }, 200);
+    });
+
+    $(".modal").on("click", function(event) {
+
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+
+            $this.removeClass('show');
+        $("body").removeClass('no-scroll');
+
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
     });
 
 });
