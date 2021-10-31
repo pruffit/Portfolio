82e0f6a34d7e9c8448a1e7c08eaf1e54e8956479
Pruffit <kotlaevdanil@gmail.com>
2021-10-31 18:35:28 +0400

    Adaptive v 1.1

--- js/app.js
+++ js/app.js
@@ -119,4 +119,15 @@ $(function() {
         currentSlider.slick("slickNext");
     });
 
+    /*MOBILE NAV*/
+
+    const navToggle = $("#navToggle");
+    const nav = $("#nav");
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
 });
